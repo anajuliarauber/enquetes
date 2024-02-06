86f0c6340309f2ca6f964c1fec9038627064a0a7
anajuliarauber <anajuliarauber@hotmail.com>
2024-02-06 19:43:46 -0300

    feat: add create polls

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,18 +1,32 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
 
 const app = fastify()
+const prisma = new PrismaClient()
 
 app.listen({ port: 3333 }).then(() => {
   console.log('server running on port :3333')
 })
 
-app.get("/hello", () => {
-  return 'Hello'
+app.post("/polls", async (request, reply) => {
+  const { body } = request
+
+  const createPollBody = z.object({
+    title: z.string()
+  })
+
+  const { title } = createPollBody.parse(body)
+
+  const poll = await prisma.poll.create({
+    data: {
+      title
+    }
+  })
+  return reply.status(201).send({ pollId: poll.id })
 })
 
-
 // https
-// post polls
 // get polls/:id
 // post polls/:id/votes
 
