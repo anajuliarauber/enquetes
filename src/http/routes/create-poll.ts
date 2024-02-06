335cd41c0c7fbbc7ddd9038237e12cbaaa9ef7df
anajuliarauber <anajuliarauber@hotmail.com>
2024-02-06 20:26:56 -0300

    feat: add poll options into create poll method

--- src/http/routes/create-poll.ts
+++ src/http/routes/create-poll.ts
@@ -7,16 +7,26 @@ export async function createPoll(app: FastifyInstance) {
     const { body } = request
 
     const createPollBody = z.object({
-      title: z.string()
+      title: z.string(),
+      options: z.array(z.string())
     })
 
-    const { title } = createPollBody.parse(body)
+    const { title, options } = createPollBody.parse(body)
 
+    // insert data into 2 tables (polls and pollOption) at the same time
     const poll = await prisma.poll.create({
       data: {
-        title
+        title,
+        options: {
+          createMany: {
+            data: options.map(option => {
+              return { title: option }
+            })
+          }
+        }
       }
     })
+
     return reply.status(201).send({ pollId: poll.id })
   })
 }
