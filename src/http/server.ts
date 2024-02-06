f8e8a1de9b0db16928158d87190fba0307650326
anajuliarauber <anajuliarauber@hotmail.com>
2024-02-06 20:08:27 -0300

    refactor: separate create poll from server.ts

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,30 +1,13 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
-import { z } from 'zod'
+import { createPoll } from './routes/create-poll'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
 app.listen({ port: 3333 }).then(() => {
   console.log('server running on port :3333')
 })
 
-app.post("/polls", async (request, reply) => {
-  const { body } = request
-
-  const createPollBody = z.object({
-    title: z.string()
-  })
-
-  const { title } = createPollBody.parse(body)
-
-  const poll = await prisma.poll.create({
-    data: {
-      title
-    }
-  })
-  return reply.status(201).send({ pollId: poll.id })
-})
+app.register(createPoll)
 
 // https
 // get polls/:id
